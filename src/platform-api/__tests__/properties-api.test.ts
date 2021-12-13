6e43600af0d6add62cfad030555b6a520b9ba03f
abrahamseni <abraham@softwareseni.com>
2021-12-13 14:37:57 +0700

    test

--- src/platform-api/__tests__/properties-api.test.ts
+++ src/platform-api/__tests__/properties-api.test.ts
@@ -1,32 +1,32 @@
-import { configurationAppointmentsApiService } from "../configuration-api";
-import { ListItemModel } from "@reapit/foundations-ts-definitions";
+import { PropertyModelPagedResult } from "@reapit/foundations-ts-definitions";
 import { mockBrowserSession } from "../__mocks__/session";
+import { getAllProperties } from "../properties";
 
 const mockedFetch = jest.spyOn(window, "fetch");
 const mockConfigurationAppointments = [
   {
-    id: "some_id",
-    value: "some_value",
+    pageNumber: 8,
+    pageSize: 25,
   },
-] as ListItemModel[];
+] as PropertyModelPagedResult[];
 
-describe("configurationAppointmentsApiService", () => {
+describe("getAllProperties", () => {
   it("should return a response from the config service", async () => {
     mockedFetch.mockReturnValueOnce({
       json: jest.fn(() => mockConfigurationAppointments),
     } as any);
-    expect(
-      await configurationAppointmentsApiService(mockBrowserSession)
-    ).toEqual(mockConfigurationAppointments);
+    expect(await getAllProperties(mockBrowserSession)).toEqual(
+      mockConfigurationAppointments
+    );
     expect(mockedFetch).toHaveBeenCalledTimes(1);
   });
 
   it("should catch an error if no response from config service", async () => {
     const errorSpy = jest.spyOn(console, "error");
     mockedFetch.mockReturnValueOnce(undefined as any);
-    await configurationAppointmentsApiService(mockBrowserSession);
+    await getAllProperties(mockBrowserSession);
     expect(errorSpy).toHaveBeenCalledWith(
-      "Error fetching Configuration Appointment Types",
+      "Error fetching Properties",
       new Error("No response returned by API")
     );
   });
