beef097b47f4b272fbc586c07ea426d8e359af7b
abrahamseni <abraham@softwareseni.com>
2021-12-13 14:39:41 +0700

    refactor configurationAppointmetsApiService

--- src/components/pages/authenticated.tsx
+++ src/components/pages/authenticated.tsx
@@ -1,33 +1,35 @@
 import React, { useEffect, FC, useState } from "react";
 import { Title, Subtitle, BodyText } from "@reapit/elements";
-import { useReapitConnect } from "@reapit/connect-session";
-import { reapitConnectBrowserSession } from "../../core/connect-session";
-import { configurationAppointmentsApiService } from "../../platform-api/configuration-api";
-import { ListItemModel } from "@reapit/foundations-ts-definitions";
+// import { useReapitConnect } from "@reapit/connect-session";
+// import { reapitConnectBrowserSession } from "../../core/connect-session";
+// import { configurationAppointmentsApiService } from "../../platform-api/configuration-api";
+// import { ListItemModel } from "@reapit/foundations-ts-definitions";
 import { Link } from "react-router-dom";
+import { useGetAppointmentConfigType } from "../../utils/hooks/api";
 
 export type AuthenticatedProps = {};
 
 export const Authenticated: FC<AuthenticatedProps> = () => {
-  const { connectSession } = useReapitConnect(reapitConnectBrowserSession);
-  const [appointmentConfigTypes, setAppointmentConfigTypes] = useState<
-    ListItemModel[]
-  >([]);
+  // const { connectSession } = useReapitConnect(reapitConnectBrowserSession);
+  // const [appointmentConfigTypes, setAppointmentConfigTypes] = useState<
+  //   ListItemModel[]
+  // >([]);
 
-  useEffect(() => {
-    const fetchAppoinmentConfigs = async () => {
-      if (!connectSession) return;
-      const serviceResponse = await configurationAppointmentsApiService(
-        connectSession
-      );
-      if (serviceResponse) {
-        setAppointmentConfigTypes(serviceResponse);
-      }
-    };
-    if (connectSession) {
-      fetchAppoinmentConfigs();
-    }
-  }, [connectSession]);
+  // useEffect(() => {
+  //   const fetchAppoinmentConfigs = async () => {
+  //     if (!connectSession) return;
+  //     const serviceResponse = await configurationAppointmentsApiService(
+  //       connectSession
+  //     );
+  //     if (serviceResponse) {
+  //       setAppointmentConfigTypes(serviceResponse);
+  //     }
+  //   };
+  //   if (connectSession) {
+  //     fetchAppoinmentConfigs();
+  //   }
+  // }, [connectSession]);
+  const appointmentConfigTypes = useGetAppointmentConfigType();
 
   console.log("Appointment Config Types are: ", appointmentConfigTypes);
   return (
--- src/utils/hooks/api.ts
+++ src/utils/hooks/api.ts
@@ -2,52 +2,79 @@ import {
   useReapitConnect,
   ReapitConnectSession,
 } from "@reapit/connect-session";
-import {
-  AppointmentModelPagedResult,
-  PropertyModelPagedResult,
-} from "@reapit/foundations-ts-definitions";
-import { useQuery } from "react-query";
-import axios from "../../axios";
+// import {
+//   AppointmentModelPagedResult,
+//   PropertyModelPagedResult,
+// } from "@reapit/foundations-ts-definitions";
+// import { useQuery } from "react-query";
+// import axios from "../../axios";
 import { reapitConnectBrowserSession } from "../../core/connect-session";
+import { useEffect, useState } from "react";
+import { ListItemModel } from "@reapit/foundations-ts-definitions";
+import { configurationAppointmentsApiService } from "../../platform-api/configuration-api";
 
-async function getProperties({
-  queryKey,
-}: {
-  queryKey: [string, ReapitConnectSession | null, number, any];
-}): Promise<PropertyModelPagedResult> {
-  let params;
-  if (!queryKey[3]) {
-    params = "";
-  } else {
-    params = Object.keys(queryKey[3]).reduce((obj, el) => {
-      return obj.concat(`${el}=${queryKey[3][el]}`);
-    }, "&");
-  }
+// async function getProperties({
+//   queryKey,
+// }: {
+//   queryKey: [string, ReapitConnectSession | null, number, any];
+// }): Promise<PropertyModelPagedResult> {
+//   let params;
+//   if (!queryKey[3]) {
+//     params = "";
+//   } else {
+//     params = Object.keys(queryKey[3]).reduce((obj, el) => {
+//       return obj.concat(`${el}=${queryKey[3][el]}`);
+//     }, "&");
+//   }
 
-  try {
-    const res = await axios.get(
-      `properties/?pageNumber=${queryKey[2]}${params}`,
-      {
-        headers: {
-          Authorization: `Bearer ${queryKey[1]?.accessToken}`,
-        },
-      }
-    );
-    return res.data;
-  } catch (error) {
-    console.error(error);
-    throw new Error("Error getting properties");
-  }
-}
+//   try {
+//     const res = await axios.get(
+//       `properties/?pageNumber=${queryKey[2]}${params}`,
+//       {
+//         headers: {
+//           Authorization: `Bearer ${queryKey[1]?.accessToken}`,
+//         },
+//       }
+//     );
+//     return res.data;
+//   } catch (error) {
+//     console.error(error);
+//     throw new Error("Error getting properties");
+//   }
+// }
 
-export const useGetPropertiesBy = (page: number, options?: any) => {
+// export const useGetPropertiesBy = (page: number, options?: any) => {
+//   const { connectSession } = useReapitConnect(reapitConnectBrowserSession);
+
+//   const properties = useQuery<
+//     PropertyModelPagedResult,
+//     Error,
+//     PropertyModelPagedResult,
+//     [string, ReapitConnectSession | null, number, any]
+//   >(["getProperties", connectSession, page, options], getProperties);
+//   return properties;
+// };
+
+export const useGetAppointmentConfigType = () => {
   const { connectSession } = useReapitConnect(reapitConnectBrowserSession);
+  const [appointmentConfigTypes, setAppointmentConfigTypes] = useState<
+    ListItemModel[]
+  >([]);
 
-  const properties = useQuery<
-    PropertyModelPagedResult,
-    Error,
-    PropertyModelPagedResult,
-    [string, ReapitConnectSession | null, number, any]
-  >(["getProperties", connectSession, page, options], getProperties);
-  return properties;
+  useEffect(() => {
+    const fetchAppoinmentConfigs = async () => {
+      if (!connectSession) return;
+      const serviceResponse = await configurationAppointmentsApiService(
+        connectSession
+      );
+      if (serviceResponse) {
+        setAppointmentConfigTypes(serviceResponse);
+      }
+    };
+    if (connectSession) {
+      fetchAppoinmentConfigs();
+    }
+  }, [connectSession]);
+
+  return appointmentConfigTypes;
 };
