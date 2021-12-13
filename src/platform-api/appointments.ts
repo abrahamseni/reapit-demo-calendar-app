e2dd6d5001834871f907c84f158bd8fef76400ee
abrahamseni <abraham@softwareseni.com>
2021-12-13 17:07:26 +0700

    feat: post appointment

--- src/platform-api/appointments.ts
+++ src/platform-api/appointments.ts
@@ -1,7 +1,8 @@
+import * as React from "react";
 import { ReapitConnectSession } from "@reapit/connect-session";
 import { AppointmentModelPagedResult } from "@reapit/foundations-ts-definitions";
 import { URLS } from "../constants/api";
-import { useQuery } from "react-query";
+import { useQuery, useMutation } from "react-query";
 import axios from "../axios";
 
 export const useGetAppointmentByNegotiator = (
@@ -40,4 +41,40 @@ export const useGetAppointmentByNegotiator = (
   );
 
   return result;
+};
+
+export const usePostNewAppointment = () => {
+  const [data, setData] = React.useState();
+
+  const actionPostAppointment = async ({
+    session,
+    body,
+  }: {
+    session: ReapitConnectSession;
+    body: any;
+  }) => {
+    // axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+    return axios
+      .post(
+        `${window.reapit.config.platformApiUrl}${URLS.APPOINTMENTS}/`,
+        body,
+        {
+          headers: {
+            Authorization: `Bearer ${session.accessToken}`,
+          },
+        }
+      )
+      .catch((err) => console.error(err));
+  };
+
+  // @ts-ignore
+  const postAppointment = useMutation(actionPostAppointment, {
+    onError: (error) => console.log(error),
+    // @ts-ignore
+    onSuccess: (data) => {
+      console.log("data:", data);
+    },
+  });
+
+  return [postAppointment, data];
 };
